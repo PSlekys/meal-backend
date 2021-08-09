04a44f37a191584ed10c47d8692a4b57c73357f6
“Petras <p.slekys@gamail.com>
2021-08-09 18:15:53 +0100

    updt

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const meals = [
-  {
-    name: "Šaltibarščiai",
-    price: 3,
-  },
-];
+let meals = [];
 
 app.get("/", (req, res) => {
   res.send(meals);
@@ -22,11 +17,17 @@ app.post("/", (req, res) => {
   }
 
   meals.push({
+    id: Math.floor(Math.random() * 100000),
     name: req.body.name.trim(),
     price: Number(req.body.price),
   });
 
   res.send({ msg: "Meal added" });
+});
+
+app.delete("/:id", (req, res) => {
+  meals = meals.filter((x) => x.id !== Number(req.params.id));
+  res.send({ msg: "Meal deleted" });
 });
 
 const port = 8080;
